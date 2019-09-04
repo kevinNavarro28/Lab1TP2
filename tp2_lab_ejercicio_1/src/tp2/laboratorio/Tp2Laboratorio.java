6450d4d0a444865347e040ba5b8b4411e29a28d0
kevinNavarro28 <maxikevin2018@gmail.com>
2019-09-04 11:22:23 -0300

    laboratorio tp2

---
+++ tp2_lab_ejercicio_1/src/tp2/laboratorio/Tp2Laboratorio.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2.laboratorio;
+
+/**
+ *
+ * @author kevin
+ */
+public class Tp2Laboratorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        
+        Persona p1=new Persona("kevin",28,38221454,"barrio 3");
+        p1.coRrer();
+        p1.anDar();
+        Deportista d1=new Deportista("juan",32,21454634,"barrio 2",23);
+        d1.anDar();
+        d1.coRrer();
+    }
+    
+}
